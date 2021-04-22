aa24f0aed29ab2b9fabda0bc629b20501d1d4b37
Psilon <pzixel@gmail.com>
2021-04-23 01:08:58 +0300

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use iced::{Button, Column, Text, Settings, Application, Executor, Command, Element, Clipboard, Container, Length, Align, HorizontalAlignment, Canvas, Rectangle, Point, Color, Size};
-use iced::canvas::{Geometry, Cursor, Path};
+use iced::{
+    canvas::{Cursor, Geometry, Path},
+    Align, Application, Button, Canvas, Clipboard, Color, Column, Command, Container, Element,
+    Executor, HorizontalAlignment, Length, Point, Rectangle, Settings, Size, Text,
+};
 use rand::random;
 
 const FIELD_SIZE: usize = 64;
@@ -9,7 +12,7 @@ type CellOwner = Option<Player>;
 #[derive(Debug, Clone, Copy)]
 pub enum Player {
     Player1,
-    Player2
+    Player2,
 }
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -43,7 +46,13 @@ impl<Message> iced::canvas::Program<Message> for DrawingPart {
             let space = Path::rectangle(Point::new(0.0, 0.0), frame.size());
             frame.fill(&space, Color::BLACK);
             if let Some(Dices(x, y)) = self.dices {
-                let dice_throw =  Path::rectangle(Point::new(0.0, 0.0), Size::new(frame.size().width/(x as f32), frame.size().height/(y as f32)));
+                let dice_throw = Path::rectangle(
+                    Point::new(0.0, 0.0),
+                    Size::new(
+                        frame.size().width / (x as f32),
+                        frame.size().height / (y as f32),
+                    ),
+                );
                 frame.fill(&dice_throw, Color::from_rgb(0., 1., 0.));
             }
         });
@@ -51,48 +60,52 @@ impl<Message> iced::canvas::Program<Message> for DrawingPart {
     }
 }
 
-
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     ThrowDices,
-    ChangeOwner { player: Player, x: usize, y: usize}
+    ChangeOwner { player: Player, x: usize, y: usize },
 }
 
-
 fn color_changed(player: Player, x: usize, y: usize) -> Message {
-    Message::ChangeOwner { player, x, y}
+    Message::ChangeOwner { player, x, y }
 }
 
 impl Application for Game {
     type Executor = iced::executor::Default;
-    type Message = Message;
     type Flags = ();
+    type Message = Message;
 
-    fn new(flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (Game{
-            state: GameState {
-                drawing: DrawingPart {
-                    dices: Default::default(),
-                    cells: Cells([[None; FIELD_SIZE]; FIELD_SIZE]),
-                    canvas: Default::default()
+    fn new(_: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            Game {
+                state: GameState {
+                    drawing: DrawingPart {
+                        dices: Default::default(),
+                        cells: Cells([[None; FIELD_SIZE]; FIELD_SIZE]),
+                        canvas: Default::default(),
+                    },
+                    roll_dice: iced::button::State::new(),
+                    current_player: Player::Player1,
                 },
-                roll_dice: iced::button::State::new(),
-                current_player: Player::Player1
-            }
-        },
-         Command::none())
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Dice wars")
     }
 
-    fn update(&mut self, message: Self::Message, clipboard: &mut Clipboard) -> Command<Self::Message> {
+    fn update(
+        &mut self,
+        message: Self::Message,
+        clipboard: &mut Clipboard,
+    ) -> Command<Self::Message> {
         match message {
             Message::ThrowDices => {
                 let new_dices: u8 = random();
-                let dice1 = (new_dices >> 4)%5 + 1;
-                let dice2 = new_dices%5 + 1;
+                let dice1 = (new_dices >> 4) % 5 + 1;
+                let dice2 = new_dices % 5 + 1;
                 self.state.drawing.dices = Some(Dices(dice1, dice2))
             },
             _ => {},
@@ -105,11 +118,15 @@ impl Application for Game {
         let content = Column::new()
             .align_items(Align::Center)
             .spacing(20)
-            .push(Button::new(&mut self.state.roll_dice, Text::new("Throw dices!"))
-                .on_press(Message::ThrowDices))
-            .push(Canvas::new(&mut self.state.drawing)
-                .width(Length::Fill)
-                .height(Length::Fill));
+            .push(
+                Button::new(&mut self.state.roll_dice, Text::new("Throw dices!"))
+                    .on_press(Message::ThrowDices),
+            )
+            .push(
+                Canvas::new(&mut self.state.drawing)
+                    .width(Length::Fill)
+                    .height(Length::Fill),
+            );
 
         Container::new(content)
             .width(Length::Fill)
